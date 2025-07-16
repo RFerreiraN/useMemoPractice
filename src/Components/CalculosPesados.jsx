22c65fd5047101b1707fcbd9288caba8c9e6a36f
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-07-16 22:53:52 +0200

    practica finalizada

--- src/Components/CalculosPesados.jsx
+++ src/Components/CalculosPesados.jsx
@@ -1,20 +1,32 @@
-import {useState} from 'react'
+import {useEffect, useMemo, useState} from 'react'
 
 export const CalculosPesados = () => {
 
   const [calculo, setCalculo] = useState([2,6,4,8,9,7,5,9])
 
-  const calcular = () => {
-    setCalculo([])
+  const calcular = (calculo) => useMemo(() =>{
+    console.log('Calculando..')
+    return calculo.reduce((a,b) => a * b , 1)
+  }, [calculo])
+
+  const addNumero = () => {
+    setCalculo([
+      ...calculo, calculo[calculo.length - 1] + 1 
+    ])
+    
   }
+
+  const [show, setShow] = useState(true)     
+
 
   return (
     <>
       <h1>Hook useMemo</h1>
       <hr />
       <h2>Calculos Pesados:</h2>
-      <p>{calculo}</p>
-      <button onClick={calcular}>Calcular</button>
+      <p>{calcular(calculo)}</p>
+      <button className='btn btn-success' onClick={addNumero}>Calcular</button>
+      <button className='btn btn-danger' onClick={() => setShow(!show)}>{ show ? ' Show' : 'Hide'}</button>
     </>
   )
 }
